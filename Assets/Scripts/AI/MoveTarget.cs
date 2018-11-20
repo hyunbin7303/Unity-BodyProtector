9038f91a2b3427037197b6fb3478e1c002a276ad
Austin <acheese1234@gmail.com>
2018-11-20 18:10:46 -0500

    testing ai path finding

---
+++ Assets/Scripts/AI/MoveTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTarget : MonoBehaviour
+{
+    public LayerMask hitLayers;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouse = Input.mousePosition;
+            Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+            RaycastHit hit;
+            if (Physics.Raycast(castPoint, out hit, Mathf.Infinity, hitLayers))
+            {
+                this.transform.position = hit.point;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AI/Node.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    // The grid
+    public int gridX;
+    public int gridY;
+
+    public bool IsWall;
+    public Vector3 Position;
+
+    public Node Parent;
+
+    public int gCost;
+    public int hCost;
+
+    public int FCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+
+    public Node(bool isWall, Vector3 pos, int gridX, int gridY)
+    {
+        this.IsWall = isWall;
+        this.Position = pos;
+        this.gridX = gridX;
+        this.gridY = gridY;
+    }
+}
