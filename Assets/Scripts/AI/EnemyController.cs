e6c6699eaf668df92d1d06a0073bdf7eb40546c9
Austin <acheese1234@gmail.com>
2018-11-22 18:37:28 -0500

    added the ai scripts

---
+++ Assets/Scripts/AI/EnemyController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                // Attack the target
+
+                // Face the target
+                FaceTarget();
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        // Get a direction vector of where the target is
+        Vector3 direction = (target.position - transform.position).normalized;
+        // Get a rotation of where the target is
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    /// <summary>
+    /// Draw a wireframe sphere for the enemy's search radius.
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
