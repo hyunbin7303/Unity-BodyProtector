31ec38ee2d5e9729e4f9808d9535a6290f86c08a
hyunbin7303 <hyunbin7303@gmail.com>
2018-11-24 16:53:25 -0500

    push

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -34,19 +34,15 @@ public class PlayerController : NetworkBehaviour
 
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("PRESSED SPACE - [FIRE METHOD]");
-            CmdFire();
+         //   CmdFire();
         }
 
-
     }
-
     // This method is used for assigning color to the main character that player is playing.
     public override void OnStartLocalPlayer()
     {
@@ -54,25 +50,6 @@ public class PlayerController : NetworkBehaviour
     }
 
 
-    /* Fire Method 
-     * Description : Used for firing bullet.
-     * Currently working on right now.
-     */
-     [Command]
-    void CmdFire()
-    {
-        // Create the Bullet from the Bullet Prefab
-        var bullet = (GameObject)Instantiate(
-            bulletPrefab,
-            bulletSpawn.position,
-            bulletSpawn.rotation);
 
-        // Add velocity to the bullet
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
 
-        // Destroy the bullet after 2 seconds
-        Destroy(bullet, 2.0f);
-
-        NetworkServer.Spawn(bullet);
-    }
 }
