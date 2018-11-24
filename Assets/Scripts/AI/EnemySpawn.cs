2de63add11835b3860ae049c80b2428bcf72d3a7
Austin <acheese1234@gmail.com>
2018-11-23 20:21:53 -0500

    [WIP] enemy nearest target.
    [TODO] sync with client-side

--- Assets/Scripts/AI/EnemySpawn.cs
+++ Assets/Scripts/AI/EnemySpawn.cs
@@ -1,40 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 /// <summary>
 /// 
 /// </summary>
-public class EnemySpawn : MonoBehaviour
+public class EnemySpawn : NetworkBehaviour
 {
     public int currentEnemyCount = 0;
     public int maxEnemyLimit = 3;
 
+    [Tooltip("Prefab of the enemy.")]
     public GameObject enemy;
+
+    [Tooltip("The time between spawning an enemy.")]
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
 
+    private bool _enableSpawning;
 
-    void Start()
+
+    void OnEnable()
     {
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        _enableSpawning = false;
     }
 
-    void Spawn()
+    public override void OnStartServer()
     {
-        if (spawnPoints.Length <= 0) {
-            return;
+        _enableSpawning = true;
+        InvokeRepeating("SpawnEnemy", spawnTime, spawnTime);
+    }
+
+    void ResetData()
+    {
+        _enableSpawning = false;
+        currentEnemyCount = 0;
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void SpawnEnemy()
+    {
+        // Check to see if the server is active
+        // and spawn the enemy once server is active
+        if (NetworkServer.active)
+        {
+            if (_enableSpawning)
+            {
+                if (spawnPoints.Length <= 0)
+                { // If no spawn points were given, then do nothing
+                    Debug.LogError("[EnemySpawn]: Please assign spawn points.");
+                    return;
+                }
+                if (currentEnemyCount >= maxEnemyLimit)
+                {
+                    return;
+                }
+
+                // Create an enemy at a random spawn point
+                int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+                // Create an instance of the enemy and spawn at the points
+                GameObject enemyObject = Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+                ++currentEnemyCount;
+
+                NetworkServer.Spawn(enemyObject);
+            }
         }
-        if (currentEnemyCount >= maxEnemyLimit) {
-            return;
+        else
+        {
+            ResetData();
         }
-
-        // Create an enemy at a random spawn point
-        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-
-        // Create an instance of the enemey and spawn at the points
-        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-
-        ++currentEnemyCount;
     }
 }
