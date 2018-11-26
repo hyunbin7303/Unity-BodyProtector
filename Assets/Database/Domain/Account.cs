a117c99da94c74f768e03135c01ca17888718c10
Austin <acheese1234@gmail.com>
2018-11-26 18:28:01 -0500

    pushing database stuff

---
+++ Assets/Database/Domain/Account.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Varlab.Database.Domain
+{
+    /// <summary>
+    /// An entity model representing the Account table.
+    /// </summary>
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+
+        public int HighScore { get; set; }
+        public int TotalKills { get; set; }
+        public int TotalDeaths { get; set; }
+        public int RoundsWon { get; set; }
+        public int RoundsLost { get; set; }
+
+
+        public Account() : this(0, null, null)
+        {
+        }
+
+        public Account(int accountID, string username, string email)
+        {
+            AccountID = accountID;
+            Username = username;
+            Email = email;
+            PasswordHash = "";
+
+            HighScore = 0;
+            TotalKills = 0;
+            TotalDeaths = 0;
+            RoundsWon = 0;
+            RoundsLost = 0;
+        }
+    }
+}
---
+++ Assets/Database/Domain/LevelScore.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Varlab.Database.Domain
+{
+    /// <summary>
+    /// Tracks the score for the current session.
+    /// </summary>
+    public class LevelScore
+    {
+        public int LevelScoreID { get; set; }
+        public int AccountID { get; set; }
+
+        public int Score { get; set; }
+        public int Kills { get; set; }
+        public int Deaths { get; set; }
+
+
+        public LevelScore() : this(0, 0)
+        {
+        }
+
+        public LevelScore(int levelScoreID, int accountID)
+        {
+            LevelScoreID = levelScoreID;
+            AccountID = accountID;
+
+            Score = 0;
+            Kills = 0;
+            Deaths = 0;
+        }
+    }
+}
