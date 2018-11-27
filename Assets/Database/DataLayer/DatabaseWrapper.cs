3742479899c2ef5e424680930a15a8f80adf8f8a
Austin <acheese1234@gmail.com>
2018-11-26 19:04:04 -0500

    Updated database stuff

--- Assets/Database/DataLayer/DatabaseWrapper.cs
+++ Assets/Database/DataLayer/DatabaseWrapper.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using Mono.Data.Sqlite;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using UnityEngine;
@@ -8,7 +10,7 @@ namespace Varlab.Database
     /// <summary>
     /// 
     /// </summary>
-    public class DatabaseWrapper
+    public class DatabaseWrapper : IDisposable
     {
         public static string DB_NAME = "bodyprotector.db";
         public static string CONN = "URI=file:" + Application.dataPath + "/Database/Source/" + DB_NAME;
@@ -16,5 +18,57 @@ namespace Varlab.Database
         private IDbConnection db;
 
 
+        public void OpenConnection()
+        {
+            try
+            {
+                db = new SqliteConnection(CONN);
+                // Open connection to database
+                db.Open();
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("[ERROR] Failed to open database. " + ex.Message);
+            }
+        }
+
+        public void CloseConnection()
+        {
+            try
+            {
+                db.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("[ERROR] Failed to close database connection. " + ex.Message);
+            }
+        }
+
+        public void ExecuteInsert(string table, string query, string[] columns, string[] args)
+        {
+            IDbCommand cmd = db.CreateCommand();
+
+            string columnFormat = "(";
+            for (int i = 0; i < columns.Length; i++)
+            {
+                columnFormat += "{" + i + "}";
+                if (i != (columns.Length - 1))
+                {
+                    columnFormat += ",";
+                }
+            }
+            columnFormat += ")";
+
+            columnFormat = string.Format(columnFormat, columns);
+        }
+
+        public void Dispose()
+        {
+            if (db != null)
+            {
+                db.Close();
+                db.Dispose();
+            }
+        }
     }
 }
