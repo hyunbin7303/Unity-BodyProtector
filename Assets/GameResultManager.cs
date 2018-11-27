9f2564f8a87b4d1b4159b74eec56196aa128706a
Austin <acheese1234@gmail.com>
2018-11-27 18:23:38 -0500

    Finish UI

--- Assets/GameResultManager.cs
+++ Assets/GameResultManager.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameResultManager : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    public void RePlaygame()
+    {
+        Debug.Log("REPLAY GAME");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("EXIT GAME PRESSED");
+        Application.Quit();
+    }
+
 }
