5381384ee5e9db1b5977392bb8782937870f7374
Austin <acheese1234@gmail.com>
2018-12-11 17:11:32 -0500

    added character status

---
+++ Assets/Scripts/Shared/CharacterStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The state of a player, character, or NPC.
+/// </summary>
+public enum CharacterStatus
+{
+    ALIVE,
+    RESCUE,
+    DEAD
+}
